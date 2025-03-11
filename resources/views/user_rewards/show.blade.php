66ada70c520c27cd06d4967bf36fcf6b7ef01065
Heitor Freitas <heitor.freitas@crmall.com.br>
2025-03-11 14:02:09 -0300

    Styled user rewards page

--- resources/views/user_rewards/show.blade.php
+++ resources/views/user_rewards/show.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 <div class="container mt-5">
-    <div class="card p-4 text-white bg-dark">
+    <div class="card p-4 text-white" style="background: rgba(0, 0, 0, 0.8); border: 2px solid #00ff00; border-radius: 15px;">
         <div class="card-title">
-            <h1 style="font-weight: 600; font-size:18px;">Recompensas do Usuário</h1>
+            <h1 style="font-weight: 600; font-size: 24px; font-family: 'Press Start 2P', cursive; color: #00ff00;">Recompensas do Usuário</h1>
         </div>
         <div class="card-body">
             @isset($userReward)
